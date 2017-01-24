6852c97b5e3155801d1c157473471104597e181f
Andrew Stilliard <andrew.stilliard@gmail.com>
2017-01-24 07:49:22 +0000

    New redesign in place, just need to add in the content and variations based on filters

---
+++ js/app.js
@@ -0,0 +1,11 @@
+
+new Vue({
+    el: '#app',
+
+    // few filters available to change content in main area
+    data: {
+        platform: 'Wordpress',
+        hosting: '',
+        control: 'max'
+    }
+});
