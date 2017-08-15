e1417fd8731d5fbb84322dbca521719a5eed733b
Andrew Stilliard <andrew.stilliard@gmail.com>
2017-08-15 18:59:12 +0100

    Replace CDN usage of unpkg with cdnjs, noticed an issue where fonts were not loading correctly from unpkg and returning 403s

--- sw.js
+++ sw.js
@@ -13,9 +13,9 @@ self.addEventListener('install', e => {
                 '/css/app.css',
                 '/js/app.js',
                 'https://fonts.googleapis.com/css?family=Bungee+Shade|Noto+Sans:400,400i,700,700i',
-                'https://unpkg.com/normalize.css@5.0.0/normalize.css',
-                'https://unpkg.com/font-awesome@4.7.0/css/font-awesome.min.css',
-                'https://unpkg.com/vue@2.1.10/dist/vue.min.js',
+                'https://cdnjs.cloudflare.com/ajax/libs/normalize/7.0.0/normalize.min.css',
+                'https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css',
+                'https://cdnjs.cloudflare.com/ajax/libs/vue/2.4.2/vue.min.js',
             ])
             .then(() => self.skipWaiting());
         })
